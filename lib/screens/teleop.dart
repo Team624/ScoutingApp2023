6e202439f6553f04768354b973ea3126689450f8
Alexander Park <me@parkalex.dev>
2023-01-12 14:26:43 -0600

    checkmark widget

--- lib/screens/teleop.dart
+++ lib/screens/teleop.dart
@@ -2,6 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:scouting_app_2023/widgets/Counter.dart';
+import 'package:scouting_app_2023/widgets/checkbox.dart';
 
 class TeleOpPage extends StatefulWidget {
   const TeleOpPage({Key? key}) : super(key: key);
@@ -23,11 +24,21 @@ class _TeleOpPageState extends State<TeleOpPage> {
         ),
         Column(
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [Counter(), Counter(), Counter()],
+          children: [
+            Counter(),
+            Counter(),
+            Counter(),
+            Checkbox(text: "sussy amogus checkmark")
+          ],
         ),
         Column(
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [Counter(), Counter(), Counter()],
+          children: [
+            Counter(),
+            Counter(),
+            Counter(),
+            Checkbox(text: "amogus checkmark 2")
+          ],
         ),
       ],
     );
