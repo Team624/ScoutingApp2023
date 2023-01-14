8bc5a45e521c8210a0f2276e9e29afcb4793eae9
Alexander Park <me@parkalex.dev>
2023-01-14 14:52:00 -0600

    experiment with new slider

--- lib/screens/teleop.dart
+++ lib/screens/teleop.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:scouting_app_2023/widgets/Counter.dart';
 import 'package:scouting_app_2023/widgets/checkbox.dart';
-import 'package:counter_button/counter_button.dart';
+// import 'package:counter_button/counter_button.dart';
+import 'package:stepper_counter_swipe/stepper_counter_swipe.dart';
 
 class TeleOpPage extends StatefulWidget {
   const TeleOpPage({Key? key}) : super(key: key);
@@ -13,6 +15,8 @@ class TeleOpPage extends StatefulWidget {
 }
 
 class _TeleOpPageState extends State<TeleOpPage> {
+  int counter = 0;
+
   @override
   Widget build(BuildContext context) {
     return Row(
@@ -25,24 +29,38 @@ class _TeleOpPageState extends State<TeleOpPage> {
         ),
         Column(
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            SizedBox(
-                child: CounterButton(
-                    count: 1, onChange: (int num) {}, loading: false),
-                width: 175,
-                height: 60),
-            CounterButton(count: 1, onChange: (int num) {}, loading: false),
-            CounterButton(count: 1, onChange: (int num) {}, loading: false),
-            Checkbox(text: "sussy amogus checkmark")
-          ],
+          children: [],
         ),
         Column(
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
-            CounterButton(count: 1, onChange: (int num) {}, loading: false),
-            CounterButton(count: 1, onChange: (int num) {}, loading: false),
-            CounterButton(count: 1, onChange: (int num) {}, loading: false),
-            Checkbox(text: "amogus checkmark 2")
+            Container(
+              padding: const EdgeInsets.all(8.0),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(8.0),
+                color: Colors.blue[300],
+              ),
+              child: StepperSwipe(
+                stepperValue: counter,
+                initialValue: 0,
+                speedTransitionLimitCount: 3, //Trigger count for fast counting
+                onChanged: (int value) {
+                  setState(() {
+                    counter = value;
+                  });
+                },
+                firstIncrementDuration: Duration(
+                    milliseconds: 250), //Unit time before fast counting
+                secondIncrementDuration: Duration(
+                    milliseconds: 100), //Unit time during fast counting
+                direction: Axis.horizontal,
+                dragButtonColor: Colors.blueAccent,
+                maxValue: 9,
+                minValue: 0,
+              ),
+              height: 90,
+              width: 200,
+            )
           ],
         ),
       ],
--- lib/widgets/Counter.dart
+++ lib/widgets/Counter.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:stepper_counter_swipe/stepper_counter_swipe.dart';
 
 class Counter extends StatefulWidget {
-  const Counter({Key? key}) : super(key: key);
+  const Counter({Key? key, onPressed}) : super(key: key);
 
   @override
   State<Counter> createState() => _CounterState();
@@ -13,50 +14,6 @@ class _CounterState extends State<Counter> {
   int counter = 0;
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: 225,
-      child: Center(
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            GestureDetector(
-              child: Container(
-                color: Colors.red[500],
-                height: 75,
-                width: 75,
-                child: Icon(Icons.remove),
-              ),
-              onTap: (() {
-                setState(() {
-                  if (counter > 0) {
-                    counter--;
-                  }
-                });
-              }),
-            ),
-            Container(
-              color: Colors.grey[300],
-              height: 50,
-              width: 50,
-              child: Center(
-                child: Text("$counter"),
-              ),
-            ),
-            GestureDetector(
-              child: Container(
-                  color: Colors.green[600],
-                  height: 75,
-                  width: 75,
-                  child: Icon(Icons.add)),
-              onTap: () {
-                setState(() {
-                  counter++;
-                });
-              },
-            )
-          ],
-        ),
-      ),
-    );
+    return Container();
   }
 }
